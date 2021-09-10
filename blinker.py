6042d8c4f6f27f55226156c3b72be62b02bbb95c
Jay Conklin <jconkli07@ccs.local>
2021-09-10 15:02:56 -0400

    added blinker

---
+++ blinker.py
@@ -0,0 +1,15 @@
+import board
+import neopixel
+import time
+
+dot = neopixel.NeoPixel(board.NEOPIXEL, 1)
+
+print("Make it red!")
+
+dot.brightness=0.1
+
+while True:
+    dot.fill((255,0,0))
+    time.sleep(.5)
+    dot.fill((0,0,255))
+    time.sleep(.5)
