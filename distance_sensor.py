15e4654be0eb954ba358444dde0ec8d70f98f59d
Jay Conklin <jconkli07@ccs.local>
2021-09-22 15:26:21 -0400

    Finished distance_sensor

---
+++ distance_sensor.py
@@ -0,0 +1,23 @@
+# Jay Conklin
+# Makes the built-in LED change colors based on how far an object is from the distance sensor.
+import adafruit_hcsr04
+import time
+import board
+import neopixel
+
+sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.D2, echo_pin=board.D3)
+dot = neopixel.NeoPixel(board.NEOPIXEL, 1)
+
+dot.brightness = 0.1
+
+while True:
+    try:
+        dist = sonar.distance
+        print((dist))
+        r = max(min(15*(20-dist),255),0)
+        g = max(min(15*(dist-20),255),0)
+        b = max(min(-abs(15*(20-dist))+225,255),0)
+        dot.fill((int(r), int(g), int(b)))
+    except RuntimeError:
+        print("Retrying!")
+    time.sleep(0.1)
